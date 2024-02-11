aead557f648ff29f49159b416ceedbea0d0cc768
cinos81 <cinos81@gmail.com>
2024-02-11 12:47:48 +0900

    feat: 마크업을 구현

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const __dirname = new URL('.', import.meta.url).pathname;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       markup: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'markup.html'),
       }
     }
   }
