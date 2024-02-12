b865c341f1d4642261ba9cd33202e326e353dd27
cinos81 <cinos81@gmail.com>
2024-02-12 15:00:05 +0900

    feat: tailwindcss 인스톨

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <a href="/markup.html">마크업 보기</a>;
+  return (
+    <a href="/markup.html" className="text-3xl font-bold underline">
+      마크업 보기
+    </a>
+  );
 }
 
 export default App;
