7ed04109f25132dcea323784e313e60d95bc406d
cinos81 <cinos81@gmail.com>
2024-02-12 15:04:34 +0900

    feat: 절대경로 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import jsconfigPaths from 'vite-jsconfig-paths';
 
 const __dirname = new URL('.', import.meta.url).pathname;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jsconfigPaths()],
   build: {
     rollupOptions: {
       markup: {
