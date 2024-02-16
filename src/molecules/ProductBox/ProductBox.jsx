e911fa0f48a486bc23d574fb10741a56df0dc1d4
cinos81 <cinos81@gmail.com>
2024-02-16 16:10:48 +0900

    feat: 푸시

--- src/molecules/ProductBox/ProductBox.jsx
+++ src/molecules/ProductBox/ProductBox.jsx
@@ -1,18 +1,46 @@
+const ProductImg = () => {
+  return (
+    <div className="mb-4 h-80">
+      <img
+        src="https://chapsheep.pockethost.io/api/files/qo3tzvf8q99iywt/tpwo5fbl3kse9ss/thumb1_CYmlaTJ49L.jpg"
+        className="h-full w-full object-cover"
+        alt="오징어"
+      />
+    </div>
+  );
+};
+
+const CartButton = () => {
+  return (
+    <button
+      type="button"
+      aria-label="장바구니 버튼"
+      className="productBox__cart-button left-productBox-cart-left top-productBox-cart-top absolute"
+    >
+      <svg role="img" width="45" height="45" viewBox="0 0 45 45 ">
+        <use href="/icons/_sprite.svg#cart"></use>
+      </svg>
+    </button>
+  );
+};
+
+const 샛별배송 = () => {
+  return <span className="text-l-sm text-gray-400">샛별배송</span>;
+};
+
+const 타이틀 = () => {
+  return <p className="text-p-base text-content">[KF365] 간편한 국산 손질 오징어 1kg</p>;
+};
+
 const ProductBox = () => {
   return (
     <div className="w-productBox-width h-productBox-height list_div-productBox">
       <a href="#" className="a__product">
         <div className="relative">
-          <div className="mb-4 h-80">
-            <img
-              src="https://chapsheep.pockethost.io/api/files/qo3tzvf8q99iywt/tpwo5fbl3kse9ss/thumb1_CYmlaTJ49L.jpg"
-              className="h-full w-full object-cover"
-              alt="오징어"
-            />
-          </div>
+          <ProductImg />
           <div className="info flex flex-col gap-2">
-            <span className="text-l-sm text-gray-400">샛별배송</span>
-            <p className="text-p-base text-content">[KF365] 간편한 국산 손질 오징어 1kg</p>
+            <샛별배송 />
+            <타이틀 />
             <div className="flex gap-2">
               <p className="text-l-lg text-accent-yellow">
                 2<span>%</span>
@@ -29,15 +57,7 @@ const ProductBox = () => {
               <span className="rounded bg-gray-100 p-1 text-l-sm text-content">한정수량</span>
             </div>
           </div>
-          <button
-            type="button"
-            aria-label="장바구니 버튼"
-            className="productBox__cart-button left-productBox-cart-left top-productBox-cart-top absolute"
-          >
-            <svg role="img" width="45" height="45" viewBox="0 0 45 45 ">
-              <use href="/icons/_sprite.svg#cart"></use>
-            </svg>
-          </button>
+          <CartButton />
         </div>
       </a>
     </div>
