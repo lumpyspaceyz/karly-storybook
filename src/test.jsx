971a1151828bc14d0aba1e666740a84979d6868d
lumpyspaceyz <yzever99@gmail.com>
2024-02-17 18:21:29 +0900

    [merge] 최신 branch와 merge

--- src/test.jsx
+++ src/test.jsx
@@ -1,9 +1,5 @@
-import ${ TM_DIRECTORY /^.+\/(.*)$/ /} from './src';
-
-/**@type{import('@storybook/react').Meta} */
-export default {
-  component: ${ TM_DIRECTORY /^.+\/(.*)$/ /},
+const Test = () => {
+  return ;
 };
 
-/**@type{import('@storybook/react').StoryObj} */
-export const 기본표시 = {};
+export default Test;
